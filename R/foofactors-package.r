55ee2e2638f8ecb82654ba6c54d880a44cdcb20d
Tesdahl <Eric.Tesdahl@specialtycare.net>
2018-04-25 13:14:09 -0500

    Trite addition to check remote access.

--- R/foofactors-package.r
+++ R/foofactors-package.r
@@ -1,4 +1,10 @@
-#' foofactors.
+#' foofactors. Make factors grate again.
+#'
+#' The first rule about factors is that we don't talk about factors.
+#' The second rule about factors is that we don't talk about factors.
+#' The third rule about factors is a reminder to everyone that the first and second rules about factors are that we don't talk about factors.
+#' And Stuff!
+#'
 #'
 #' @name foofactors
 #' @docType package
