1256ece862a1b7c78c26144151634f74fa29f053
Tesdahl <Eric.Tesdahl@specialtycare.net>
2018-05-14 14:23:37 -0500

    Trite commit to test

--- R/foofactors-package.r
+++ R/foofactors-package.r
@@ -4,7 +4,7 @@
 #' The second rule about factors is that we don't talk about factors.
 #' The third rule about factors is a reminder to everyone that the first and second rules about factors are that we don't talk about factors.
 #' And Stuff!
-#' And More Stuff!
+#' And More Stuff! YEs!
 #'
 #'
 #' @name foofactors
