69d56a088978b57e3b0c3d3ab5083973f1aa61ba
2064980 <thomas.epplinzapf@specialtycare.net>
2018-05-17 15:33:54 -0500

    Initial commit

---
+++ R/average_two.R
@@ -0,0 +1,16 @@
+#' Averages two numbers
+#'
+#'Averages two numbers.
+#'
+#' @param a number
+#' @param b number
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#'
+average_two <- function(a, b) {
+    c <- (a+b)/2
+    return(c)
+}
